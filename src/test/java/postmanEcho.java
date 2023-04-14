b72d75b8521d36a61008ce4909c32c0f90877c40
Alex00000 <angara00000@yandex.ru>
2023-04-14 14:54:10 +0300

    Change test

--- src/test/java/postmanEcho.java
+++ src/test/java/postmanEcho.java
@@ -15,7 +15,7 @@ class postmanEcho {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hello world"))
+                .body("data", equalTo("Hello World"))
         ;
     }
 }
